3bd703488a294a4ba1c644408d5db9a2b647cb97
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2026-01-08 10:59:17 +0100

    Laget komponent for hvert vilkår. (#87)

--- app/routes/behandling/inngangsvilkår.tsx
+++ app/routes/behandling/inngangsvilkår.tsx
@@ -5,11 +5,13 @@ import { useBehandlingContext } from "~/contexts/BehandlingContext";
 import { useBehandlingSteg } from "~/hooks/useBehandlingSteg";
 import { useMarkerStegFerdige } from "~/hooks/useMarkerStegFerdige";
 import type { Route } from "./+types/inngangsvilkår";
+import { VilkårInnhold } from "~/komponenter/behandling/vilkår/VilkårInnhold";
 
 export function meta(_: Route.MetaArgs) {
   return [{ title: "Inngangsvilkår" }];
 }
 
+// TODO: Rename til Vilkår?
 export default function Inngangsvilkår() {
   const { behandlingId } = useBehandlingContext();
   const [erUtfylt, settErUtfylt] = useState(false);
@@ -34,15 +36,18 @@ export default function Inngangsvilkår() {
   };
 
   return (
-    <VStack gap="4">
-      <Heading size="large">Inngangsvilkår</Heading>
-      <p>Behandling ID: {behandlingId}</p>
+    <>
+      <VilkårInnhold />
+      <VStack gap="4">
+        <Heading size="large">Inngangsvilkår</Heading>
+        <p>Behandling ID: {behandlingId}</p>
 
-      <div>
-        <Button onClick={handleNesteKlikk} disabled={!harFyltUtAlt}>
-          Neste
-        </Button>
-      </div>
-    </VStack>
+        <div>
+          <Button onClick={handleNesteKlikk} disabled={!harFyltUtAlt}>
+            Neste
+          </Button>
+        </div>
+      </VStack>
+    </>
   );
 }
