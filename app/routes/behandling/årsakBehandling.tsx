63a8f56d775427918dc53c889e32d41240ddc1e1
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-01-29 08:32:13 +0100

    Ønsker ikke legge til setSelected i depency array da dette kan gi evig loop med endring av tid og useEffect

--- app/routes/behandling/årsakBehandling.tsx
+++ app/routes/behandling/årsakBehandling.tsx
@@ -71,6 +71,7 @@ export default function ArsakBehandling() {
 
   useEffect(() => {
     setSelected(kravdato);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [kravdato]);
 
   return (
