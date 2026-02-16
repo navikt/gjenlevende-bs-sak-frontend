90a2100f41eecc7a30f46c1b76ab1e6ec024e857
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-02-16 14:51:51 +0100

    Laget komponenten Totrinnskontroll som skal vises på høyremeny. Foreløping har den kun funksjonalitet for å angre/endre status på behandling. Flyttet kode for InnfoRad til egen fil da kode for denne brukes to steder. Fjernet knapp for angre send til beslutter på brev siden.

--- app/komponenter/behandling/høyremeny/HøyreMeny.tsx
+++ app/komponenter/behandling/høyremeny/HøyreMeny.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./HøyreMeny.module.css";
+import { VStack } from "@navikt/ds-react";
 
 export const HøyreMeny: React.FC<{
   children?: React.ReactNode;
 }> = ({ children }) => {
-  return <aside className={styles.høyreMeny}>{children}</aside>;
+  return (
+    <VStack as={"aside"} className={styles.høyreMeny} gap={"space-16"}>
+      {children}
+    </VStack>
+  );
 };
