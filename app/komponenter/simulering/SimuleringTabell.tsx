f8b38f99aa5d02578068de60c18d3f22e3283d34
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-05-07 10:55:29 +0200

    Flytter iterering av data ut av table

--- app/komponenter/simulering/SimuleringTabell.tsx
+++ app/komponenter/simulering/SimuleringTabell.tsx
@@ -3,49 +3,46 @@ import { Table } from "@navikt/ds-react";
 import type { SimuleringResultat } from "~/routes/behandling/simulering";
 
 export const SimuleringTabell: React.FC<{ resultat: SimuleringResultat }> = ({ resultat }) => {
+  const periodeData = resultat.perioder.map((periode) => {
+    const tidligereUtbetalt = periode.utbetalinger.reduce((sum, u) => sum + u.tidligereUtbetalt, 0);
+    const nyttBeløp = periode.utbetalinger.reduce((sum, u) => sum + u.nyttBeløp, 0);
+    return { fom: periode.fom, tidligereUtbetalt, nyttBeløp, differanse: nyttBeløp - tidligereUtbetalt };
+  });
+
   return (
     <Table>
       <Table.Header>
         <Table.Row>
           <Table.HeaderCell />
-          {resultat.perioder.map((periode) => (
-            <Table.HeaderCell key={periode.fom}>{periode.fom}</Table.HeaderCell>
+          {periodeData.map((p) => (
+            <Table.HeaderCell key={p.fom}>{p.fom}</Table.HeaderCell>
           ))}
         </Table.Row>
       </Table.Header>
       <Table.Body>
         <Table.Row>
           <Table.HeaderCell>Tidligere utbetalt</Table.HeaderCell>
-          {resultat.perioder.map((periode) => (
-            <Table.DataCell key={periode.fom}>
-              {periode.utbetalinger.reduce((sum, u) => sum + u.tidligereUtbetalt, 0)}
-            </Table.DataCell>
+          {periodeData.map((p) => (
+            <Table.DataCell key={p.fom}>{p.tidligereUtbetalt}</Table.DataCell>
           ))}
         </Table.Row>
         <Table.Row>
           <Table.HeaderCell>Nytt beløp</Table.HeaderCell>
-          {resultat.perioder.map((periode) => (
-            <Table.DataCell key={periode.fom}>
-              {periode.utbetalinger.reduce((sum, u) => sum + u.nyttBeløp, 0)}
-            </Table.DataCell>
+          {periodeData.map((p) => (
+            <Table.DataCell key={p.fom}>{p.nyttBeløp}</Table.DataCell>
           ))}
         </Table.Row>
         <Table.Row>
           <Table.HeaderCell>Differanse</Table.HeaderCell>
-          {resultat.perioder.map((periode) => {
-            const tidligereUtbetalt = periode.utbetalinger.reduce((sum, u) => sum + u.tidligereUtbetalt, 0);
-            const nyttBeløp = periode.utbetalinger.reduce((sum, u) => sum + u.nyttBeløp, 0);
-            const differanse = nyttBeløp - tidligereUtbetalt;
-            return (
-              <Table.DataCell key={periode.fom}>
-                {differanse !== 0 && (
-                  <span style={{ color: differanse > 0 ? "var(--a-green-600)" : "var(--a-red-500)" }}>
-                    {differanse}
-                  </span>
-                )}
-              </Table.DataCell>
-            );
-          })}
+          {periodeData.map((p) => (
+            <Table.DataCell key={p.fom}>
+              {p.differanse !== 0 && (
+                <span style={{ color: p.differanse > 0 ? "var(--a-green-600)" : "var(--a-red-500)" }}>
+                  {p.differanse}
+                </span>
+              )}
+            </Table.DataCell>
+          ))}
         </Table.Row>
       </Table.Body>
     </Table>
